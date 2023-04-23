adb1038497c46d34fd9531585fee087ae2c8c574
yube01 <114811769+yube01@users.noreply.github.com>
2023-04-23 07:07:56 +0545

    footer date updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
-export default function Footer(){
-	return (
-		<div className="text-center text-lg  scale-[0.8] hover:scale-[1] duration-500 group overflow-hidden flex gap-2 items-center justify-center"  > 
-		<div className="group-hover:text-xl group-hover:animate-spin ">©</div>
-		<div className="group-hover:rotate-[360deg] "> Sijan Paudel, </div>
-		 <div className="group-hover:animate-pulse group-hover:text-blue-500 duration-[3s] cursor-pointer text-blue-500">
-		 <a href="https://en.wikipedia.org/wiki/Vikram_Samvat">2079</a>
-		 </div> 
-		 </div	>
-		);
+export default function Footer() {
+  const currentYear = new Date().getFullYear();
+  return (
+    <div className="text-center text-lg  scale-[0.8] hover:scale-[1] duration-500 group overflow-hidden flex gap-2 items-center justify-center">
+      <div className="group-hover:text-xl group-hover:animate-spin ">©</div>
+      <div className="group-hover:rotate-[360deg] "> Yubraj Adhikari, </div>
+      <div className="group-hover:animate-pulse group-hover:text-blue-500 duration-[3s] cursor-pointer text-blue-500">
+        <a href="https://en.wikipedia.org/wiki/Vikram_Samvat">{currentYear}</a>
+      </div>
+    </div>
+  );
 }
